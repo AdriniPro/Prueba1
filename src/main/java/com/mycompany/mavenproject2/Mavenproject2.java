3f101fd18f59c9844fc623efb9400cc2e85f85ce
AdriniPro <adrianosprogram123@gmail.com>
2022-09-22 13:10:11 -0500

    Agregando programa

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Julio","Guzman");
+        System.out.println("nombre : "+p1.getNombre());
+        System.out.println("nombre : "+p1.getApellido());
+        
+    }
+}
